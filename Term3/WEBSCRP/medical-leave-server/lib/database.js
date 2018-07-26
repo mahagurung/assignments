6398ed12fda842009d31d1589cc486ff76b99994
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2018-07-27 00:01:28 +0800

    Arrange all the database functions to one single file

---
+++ Term3/WEBSCRP/medical-leave-server/lib/database.js
@@ -0,0 +1,56 @@
+// Import the configurations
+const config = require('./config');
+
+// connect to the database
+const sqlite3 = require('sqlite3').verbose();
+
+const conn = new sqlite3.Database(config.db_file, function(err) {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log(`Connected to the database using file ${config.db_file}.`);
+});
+
+let getWithPromise = function(sql, params) {
+  return new Promise(function(resolve, reject) {
+    conn.serialize(function () {
+      conn.get(sql, params, function(err, row) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  });
+};
+
+let runWithPromise = function(sql, params) {
+  return new Promise(function(resolve, reject) {
+    conn.serialize(function () {
+      conn.run(sql, params, function(err) {
+        if (err) {
+            reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  });
+};
+
+module.exports.conn = conn;
+module.exports.getWithPromise = getWithPromise;
+module.exports.runWithPromise = runWithPromise;
+
+/**
+ * Close database connection
+ * Need to do this just before shutdown
+
+conn.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Closed the database connection.');
+});
+*/
