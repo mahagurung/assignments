f2860865efaacea773097bc2fccbc886a8b31b9f
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2018-07-27 15:29:52 +0800

    Dynamically select the column names for leave request update api

--- Term3/WEBSCRP/medical-leave-server/server.js
+++ Term3/WEBSCRP/medical-leave-server/server.js
@@ -17,15 +17,13 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/images', express.static('images'));
 
-
 // Success and failure empty json objects
 const failure_default_json = { success: 0};
-
 
 /**
  * Get employee details by id
  */
-app.get('/employee/:id', (req, res) => {
+app.get('/employee/:id', function(req, res) {
   let sql = `SELECT id, first_name, last_name, picture, designation FROM employees WHERE status = 1 and id = ?`;
   let params = [req.params.id]
 
@@ -51,6 +49,35 @@ app.get('/employee/:id', (req, res) => {
   }, function(err) {
     console.error(err.message);
     // throw new Error(reason); Later add a global error handling function and uncomment this
+  });
+});
+
+/**
+ * Get the role of the employee by id
+ */
+app.get('/employee/role/:id', function(req, res) {
+  let sql = `SELECT count(*) as c FROM employees WHERE supervisor = ?`;
+  let params = [req.params.id]
+
+  db.getWithPromise(sql, params).then(function(row) {
+    let result;
+
+    if (row.c > 0) {
+      result = {
+        success: 1,
+        role: 'supervisor'
+      };
+    } else {
+      result = {
+        success: 1,
+        role: 'employee'
+      };
+    }
+
+    helper.debug_print(`${req.originalUrl}: ${JSON.stringify(row)}`);
+    res.send(result);
+  }, function(err) {
+    console.error(err.message);
   });
 });
 
@@ -85,7 +112,7 @@ app.route('/medicalleave/:id')
   .get((req, res) => {
     // Return leave details
     helper.debug_print(req.params.id);
-    let sql = `SELECT id, remarks, employee_id, from_date, to_date, status, mc FROM medical_leaves WHERE id = ?`;
+    let sql = `SELECT id, employee_id, remarks, from_date, to_date, status, mc, clinic_id FROM medical_leaves WHERE id = ?`;
     let params = [req.params.id];
 
     db.getWithPromise(sql, params).then(function(row) {
@@ -115,9 +142,28 @@ app.route('/medicalleave/:id')
   })
 
   .put((req, res) => {
-    // Update leave details
-    let sql = `UPDATE medical_leaves SET remarks = ?, from_date = ?, to_date = ?, status = ?, clinic_id = ?, mc = ? WHERE id = ?`;
-    let params = [req.body.remarks, req.body.from_date, req.body.to_date, req.body.status, req.body.clinic_id, req.body.mc, req.params.id];
+    // Generate the query based on the input params
+    let sql = 'UPDATE medical_leaves SET';
+    let fields = ['employee_id', 'remarks', 'from_date', 'to_date', 'status', 'mc', 'clinic_id'];
+    let params = []
+
+    fields.forEach(function(field) {
+      let value = req.body[field];
+
+      if(value) {
+        sql += ` ${field}=?,`;
+        params.push(value);
+      }
+    });
+
+    sql += ' WHERE id = ?';
+
+    // Find the last , and slice it off
+    let pos = sql.lastIndexOf(',');
+    sql = `${sql.slice(0, pos)} ${sql.slice(pos+1)}`;
+
+    // Push the leave request id to the params
+    params.push(req.params.id);
 
     db.runWithPromise(sql, params).then(function(that) {
       let result = {
